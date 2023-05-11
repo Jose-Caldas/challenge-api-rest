466edc09d1e4743d604fcaa8055cd7f80c06bacf
Jose Caldas <jrcaldas@yahoo.com.br>
2023-05-11 15:06:33 -0300

    feat: update routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,43 @@
+import express, { Request, Response } from "express";
 import "dotenv/config";
-import { server } from "./server/Server";
+import cors from "cors";
+import conn from "./db/conn";
+import BookModel, { IBook } from "./models/Book";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/books", async (_, res: Response) => {
+  try {
+    const books = await BookModel.find();
+    res.status(200).json({ books, msg: "Livros carregados com sucesso!" });
+  } catch (error) {
+    res.status(404).json({ msg: "Nenhum livro encontrado!" });
+  }
+});
+
+app.post("/books", async (req: Request, res: Response) => {
+  try {
+    const { title, author, isbnNumber } = req.body;
+    const newBook: IBook = new BookModel({
+      title,
+      author,
+      isbnNumber,
+    });
+
+    await newBook.save();
+    res.status(201).json({ newBook, msg: "Livro criado com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao criar livro!" });
+  }
+});
+
+conn();
 
 const PORT = process.env.PORT || process.env.LOCAL_PORT;
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+export default app;
